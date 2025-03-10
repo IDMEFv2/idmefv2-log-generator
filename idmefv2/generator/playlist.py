d113f08d78d23b9bbfd4b832e8268d0f7cd8fca4
François Déchelle <fdechelle@teclib.com>
2025-03-10 18:40:36 +0100

    Change schema, introduce inner class

--- idmefv2/generator/playlist.py
+++ idmefv2/generator/playlist.py
@@ -3,64 +3,72 @@ import random
 import time
 import yaml
 from schema import Schema, Or, Optional #, SchemaError
-from jinja2 import Environment #, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Template
 from .player import Player
+from .funs import HELPER_FUNS
 
+# pylint: disable=too-few-public-methods
 class PlayList:
     _SCHEMA = Schema({
         'playlist': {
-            "mode": Or('sequential', 'random'),
-            'delay': int,
+            Optional('mode', default='sequential'): Or('sequential', 'random'),
+            Optional('delay', default=0): int,
             Optional('repeat', default=False): bool,
             'templates': [
                 {
                     Or('file','string', only_one=True): str,
-                    Optional('vars'): dict,
+                    Optional('vars', default={}): dict,
                 },
             ],
         },
     })
 
     @classmethod
-    def _validate(cls, data: dict):
-        cls._SCHEMA.validate(data)
+    def _validate(cls, data: dict) -> dict:
+        return cls._SCHEMA.validate(data)
 
     def __init__(self, f : io.TextIOBase):
         y = yaml.safe_load(f)
-        PlayList._validate(y)
-        playlist = y['playlist']
+        validated = PlayList._validate(y)
+        playlist = validated['playlist']
         self._mode = playlist['mode']
         self._delay = playlist['delay']
         self._repeat = playlist['repeat']
         self._templates = playlist['templates']
 
-    def _load_templates(self):
-        loaded_templates = []
-        env = Environment()
-        for t in self._templates:
-            l = {}
-            if 'vars' in t:
-                l['vars'] = t['vars']
-            if 'string' in t:
-                l['template'] = env.from_string(t['string'])
-            loaded_templates.append(l)
-        return loaded_templates
+    class _Track:
+        def __init__(self, template: Template, vars_dict: dict ):
+            self._template = template
+            self._vars = vars_dict
 
-    def _play_once(self, templates, player: Player):
+        def render(self) -> str:
+            return self._template.render(self._vars)
+
+    def _load_tracks(self, template_path: list) -> list:
+        env = Environment(loader=FileSystemLoader(template_path))
+        env.globals.update(HELPER_FUNS)
+        tracks = []
+        for t in self._templates:
+            if 'string' in t:
+                template = env.from_string(t['string'])
+            else:
+                template = env.get_template(t['file'])
+            tracks.append(self._Track(template, t['vars']))
+        return tracks
+
+    def _play_once(self, tracks: list, player: Player):
         if self._mode == 'random':
-            random.shuffle(templates)
-        templates_last = len(templates) - 1
-        for index, template in enumerate(templates):
-            v = template['vars'] if 'vars' in template else {}
-            rendered = template['template'].render(v)
-            player.play(rendered)
-            if index < templates_last or self._repeat:
+            random.shuffle(tracks)
+        last = len(tracks) - 1
+        for index, track in enumerate(tracks):
+            player.play(track.render())
+            if index < last or self._repeat:
                 time.sleep(self._delay)
 
-    def play(self, player: Player):
-        loaded_templates = self._load_templates()
+    def play(self, player: Player, template_path: list):
+        tracks = self._load_tracks(template_path)
         if self._repeat:
             while True:
-                self._play_once(loaded_templates, player)
+                self._play_once(tracks, player)
         else:
-            self._play_once(loaded_templates, player)
+            self._play_once(tracks, player)
