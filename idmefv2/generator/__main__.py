50bbeb1bbd2e770778f645b47823e7e5805d4f31
François Déchelle <fdechelle@teclib.com>
2025-03-10 18:41:04 +0100

    Add player classes automatic discovery, complete options parsing

--- idmefv2/generator/__main__.py
+++ idmefv2/generator/__main__.py
@@ -1,26 +1,56 @@
 import argparse
+import importlib
+import inspect
 from .playlist import PlayList
-from .player import PrintPlayer
+from .player import Player
 
-def parse_options():
+def _get_player_classes() -> dict:
+    player_classes = {}
+    player_module = importlib.import_module('.player', 'idmefv2.generator')
+    for name in dir(player_module):
+        a = getattr(player_module, name)
+        if inspect.isclass(a) and not inspect.isabstract(a) and issubclass(a, Player):
+            player_classes[a.__name__] = a
+    return player_classes
+
+def parse_options(players_classes: list):
     '''
     Parse command line options
 
     Returns:
         Namespace: parsed command line options
     '''
-    parser = argparse.ArgumentParser(description='Play the playlist')
-    parser.add_argument('-p', '--player', help='list player', dest='player_name')
-    parser.add_argument('play_list', nargs='+')
+    description = """
+play a playlist defined by a .yaml file
+
+playlist entries are jinja2 templates
+
+playing a template means rendering it and passing the result to a player
+available player Python classes are:
+"""
+    for name, cls in players_classes.items():
+        description += f"- {name:15} {cls.__doc__}\n"
+    parser = argparse.ArgumentParser(description=description,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('playlist', help='yaml file defining the play list')
+    parser.add_argument('-t', '--template_path', help='list of directories containing templates',
+                        default='', dest='template_path')
+    parser.add_argument('-p', '--player_class', help='player Python class', dest='player_class')
+    for cls in players_classes.values():
+        cls.add_argument(parser)
     return parser.parse_args()
 
 def _main():
-    options = parse_options()
-    filename = options.play_list[0]
-    with open(filename, 'r', encoding="utf-8") as f:
-        play_list = PlayList(f)
-    player = PrintPlayer()
-    play_list.play(player)
+    players_classes = _get_player_classes()
+    options = parse_options(players_classes)
+
+    player_class = players_classes[options.player_class]
+    player = player_class(options)
+
+    template_path = options.template_path.split(':')
+    with open(options.playlist, 'r', encoding="utf-8") as f:
+        playlist = PlayList(f)
+        playlist.play(player, template_path)
 
 if __name__ == '__main__':
     _main()
